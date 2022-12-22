516999d2cda9750d021ea19084c6b5e6014f6524
Adrian Lakatos <lakatos.adrian@mav-szk.hu>
2022-12-22 11:02:03 +0100

    PostCollectionView stack

--- PostApp/Presentation/Scenes/PostCollection/PostCollectionView.swift
+++ PostApp/Presentation/Scenes/PostCollection/PostCollectionView.swift
@@ -1,21 +1,35 @@
 import UIKit
 
 final class PostCollectionView: UIViewController {
+    private var textLabel: UILabel = .init(frame: .zero)
+    
+    override func loadView() {
+        super.loadView()
+        layout()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print("PostCollectionView - viewDidLoad")
+        style()
         
-        APIClient.shared.getPosts(byPage: 0) { result in
-            switch result {
-            case .success(let posts):
-                print(posts)
-                break
-            case .failure(let error):
-                print(error)
-            }
-        }
+        navigationItem.title = "PostCollectionView"
+        textLabel.text = "PostCollectionView"
     }
 }
 
+extension PostCollectionView {
+    private func layout() {
+        textLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(textLabel)
+        
+        NSLayoutConstraint.activate([
+            textLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            textLabel.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor)
+        ])
+    }
+    
+    private func style() {
+        view.backgroundColor = .systemBackground
+        textLabel.textColor = .label
+    }
+}
