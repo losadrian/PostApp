e0b5565ecb2d72fc8f9665074fbc1c6518b48c1d
Adrian Lakatos <lakatos.adrian@mav-szk.hu>
2022-12-22 10:37:46 +0100

    Missing network call was made, TODO: APIClient call

--- PostApp/Networking/APIClient.swift
+++ PostApp/Networking/APIClient.swift
@@ -15,7 +15,22 @@ final class APIClient: APIClientType {
 // MARK: - PostRepository
 extension APIClient: PostRepository {
     func getPosts(byPage page: Int, completion: @escaping (Result<[PostApiModel], Error>) -> Void) {
-        //TODO: missing network call
+        let additionalQueryItems: [String: String] = ["limit":"50","page":String(page)]
+        
+        let getPostsByPagePath = PathBuilder().post().build()
+        let getPostsByPageRequest = RequestBuilder(networkMethod: .GET, path: getPostsByPagePath)
+            .queryItems(additionalQueryItems)
+            .withAuth()
+            .build()
+        let getPostsByPagePublisher = NetworkPublisher(urlRequest: getPostsByPageRequest).anyPublisher(type: PostListByPageResponseApiModel.self)
+        
+        cancellationToken = getPostsByPagePublisher.sink(receiveCompletion: { cancellableCompletion in
+            if case let .failure(error as APIError) = cancellableCompletion {
+                completion(.failure(error))
+            }
+        }, receiveValue: { getPostsByPageResponse in
+            completion(.success(getPostsByPageResponse.data))
+        })
     }
     
     
--- PostApp/Presentation/Scenes/PostCollection/PostCollectionView.swift
+++ PostApp/Presentation/Scenes/PostCollection/PostCollectionView.swift
@@ -1,13 +1,23 @@
 import UIKit
 
 final class PostCollectionView: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("PostCollectionView - viewDidLoad")
+        
+        //FIXME: where is the request going and why is it not coming back?
+        let apiClient = APIClient()
+        apiClient.getPosts(byPage: 0) { result in
+            switch result {
+            case .success(let posts):
+                print(posts)
+                break
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
-
-
 }
 
